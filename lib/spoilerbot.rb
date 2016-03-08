0e27772d21c22d8cce50849d09851dabc6d56abc
jimsorock <jimmy.muga@gmail.com>
2016-03-08 03:13:08 -0800

    revert

--- lib/spoilerbot.rb
+++ lib/spoilerbot.rb
@@ -212,6 +212,7 @@ module SpoilerBot
       status 200
       a = Array.new(1) { Hash.new }
       a[0]['image_url'] = @card_url
+      a[1]['text'] = 'cool'
       reply = {text: 'Random Card from Latest Set', attachments: a}
       return reply.to_json
     end
